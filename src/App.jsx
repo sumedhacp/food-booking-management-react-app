88781aa774bd7705f559b0014ac14fb6e4038ac0
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-15 12:39:56 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 import './App.css'
-
+import Home from './components/Home'
+import AddVendor from './components/AddVendor'
 import AddStall from './components/AddStall'
+import AddFood from './components/AddFood'
+import ViewVendor from './components/ViewVendor'
+import View from './components/ViewStall'
 import ViewFood from './components/ViewFood'
 
 function App() {
@@ -9,8 +16,21 @@ function App() {
 
   return (
     <>
-      <AddStall />
-      <ViewFood />
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/addVendor' element={<AddVendor />} />
+          <Route path='/addStall' element={<AddStall />} />
+          <Route path='/addFood' element={<AddFood />} />
+          <Route path='/viewVendor' element={<ViewVendor />} />
+          <Route path='/viewStall' element={<View />} />
+          <Route path='/viewFood' element={<ViewFood />} />
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
