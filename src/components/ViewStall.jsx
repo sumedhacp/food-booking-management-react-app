c96e82cece79c909bf66e0a80124e2fc0a686c47
ARCHANA-20279 <archana552m@gmail.com>
2026-07-15 12:29:16 +0530

    Updated

--- src/components/ViewStall.jsx
+++ src/components/ViewStall.jsx
@@ -7,7 +7,7 @@ const View = () => {
     const [data, changeData] = useState([])
 
     const fetchData = () => {
-        axios.get("http://localhost:3000/view-stall")
+        axios.post("http://localhost:3000/view-stall")
             .then((response) => {
                 changeData(response.data)
             })
